427ebe9cf3fa6baf03add0ae20641c3042605af6
Ignacio <ignaciohg2021@gmail.com>
2025-08-06 00:37:22 -0500

    sexto index vercel commit: iniciando el proyecto

--- api/index.py
+++ api/index.py
@@ -1,1 +1,9 @@
-from flaskr.index import app
+import sys
+import os
+
+# Agrega la raíz del proyecto al path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+# Importa la app desde index.py
+from index import app
+
