55acd1b62f0b756b08c72a70a4f7c24aeeaa261c
Ignacio <ignaciohg2021@gmail.com>
2025-08-06 19:44:17 -0500

    septimo commit: iniciando el proyecto

--- api/index.py
+++ api/index.py
@@ -1,9 +1,6 @@
 import sys
 import os
-
-# Agrega la raíz del proyecto al path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-# Importa la app desde index.py
 from index import app
 
